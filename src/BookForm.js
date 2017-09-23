201e246549e12568eba6850707dfaf90bdb8b985
Jessica OBrien <obrien.jessica6@gmail.com>
2017-09-23 00:46:23 -0400

    adding more features to BookForm

--- src/BookForm.js
+++ src/BookForm.js
@@ -5,6 +5,29 @@ class BookForm extends React.Component {
         return (
             <form classname="ui form">
                 <h1>Add new book</h1>
+
+                <div className="field">
+                    <label htmlFor="title">Title</label>
+                    <input
+                        id="title"
+                        />
+                  </div>
+
+                  <div className="field">
+                    <label htmlFor="cover">Cover URL</label>
+                    <input
+                        id="cover"
+                        />
+                  </div>
+
+
+                  <div className="field">
+                    <img src="" alt="cover" className="ui small bordered image"/>
+                  </div>
+
+                  <div className="field">
+                      <button className="ui primary button">Save</button>
+                </div>
              </form>
         );
     }
